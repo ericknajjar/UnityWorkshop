40e076799f0a6b400ce93205f98a85ac4cf4f4e3
Erick Najjar <enajjar@gazeus.com>
2018-09-10 18:09:07 -0300

    atualizei a easyInject

--- Assets/Bootstrap.cs
+++ Assets/Bootstrap.cs
@@ -5,19 +5,20 @@ using EasyInject.IOC;
 using EasyInject.IOC.extensions;
 using System.Reflection;
 using UniRx;
+using EasyInject.Engine.Runtime;
+
+[HasBindings]
+public class Bootstrap : MonoBehaviour
+{
 
 
-
-public class Bootstrap : MonoBehaviour {
-
-  
     IBindingContext m_context;
 
     // Use this for initialization
     void Start()
     {
         var assembly = Assembly.GetAssembly(typeof(Bootstrap));
-        var provider = new EasyInject.Engine.Runtime.ReflectiveBindingFinder(true,new Assembly[] { assembly });
+        var provider = new EasyInject.Engine.Runtime.ReflectiveBindingFinder(true, new Assembly[] { assembly });
         var factory = new EasyInject.Engine.Runtime.ReflectiveBindingContextFactory(provider);
 
         m_context = factory.CreateContext();
@@ -25,12 +26,10 @@ public class Bootstrap : MonoBehaviour {
         //var up = UniRx.Observable.EveryUpdate().Where((u)=> Input.GetKeyDown(KeyCode.KeypadPlus)).Select((u)=> true);
         //var down = UniRx.Observable.EveryUpdate().Where((u) => Input.GetKeyDown(KeyCode.KeypadMinus)).Select((u) => false);
 
-       // m_context.Get<Turret>();
+        // m_context.Get<Turret>();
 
-        m_context.Get<Turret>(InnerBindingNames.Empty,Vector2.zero);
+        m_context.Get<Turret>(InnerBindingNames.Empty, Vector2.zero);
     }
-
-
 
 
 }
--- Assets/Turret/Turret.cs
+++ Assets/Turret/Turret.cs
@@ -4,20 +4,21 @@ using EasyInject.Engine.Runtime;
 using EasyInject.IOC.extensions;
 using UnityEngine;
 using UniRx;
+using EasyInject.IOC;
 
 [HasBindings]
 public class Turret : EntityComponent {
 
 
     [BindingProvider(DependencyCount = 1)]
-    private static Turret CreateInstance(IGameplayInput input, Vector2 pos){
+    private static Turret CreateInstance(IBindingContext context, Vector2 pos){
         var prefab = Resources.Load<Turret>("Turret/Turret");
 
         var go = GameObject.Instantiate(prefab.gameObject, pos, Quaternion.identity);
 
         var turret = go.GetComponent<Turret>();
 
-        turret.Entity.Context.Bind<IGameplayInput>().To(input);
+        turret.Entity.Context.FallBack(context);
 
         return turret;
     }
